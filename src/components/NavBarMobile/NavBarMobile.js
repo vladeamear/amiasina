a46ee288385b12853d3b8fde6dd43501435897b3
Vladimir Miasin <looser.s.usami@gmail.com>
2022-03-13 12:46:29 +0300

    fix 13.03.22: minor fixes + added css folder

--- src/components/NavBarMobile/NavBarMobile.js
+++ src/components/NavBarMobile/NavBarMobile.js
@@ -15,27 +15,27 @@ export const NavBarMobile = () => {
     
 
     return(<>
-    <header className="header-mobile">
+    <header className="mobile__header">
         <ThemeToggle />
         <button
             className={
-                isOpen ? 'menu-mobile-toggle open' : 'menu-mobile-toggle'
+                isOpen ? 'mobile-menu__toggle open' : 'mobile-menu__toggle'
             }
             onClick={toggle}
         >
-            <div className="menu-mobile-toggle__burger"></div>
+            <div className="mobile-menu__toggle__burger"></div>
         </button>
     </header>
-        <nav className={isOpen ? 'mobile-nav nav-shown' : 'mobile-nav'}>
-            <div className="menu-mobile-block">
-                <NavLink className="menu-link" to="/" onClick={hide}>Обо мне</NavLink>
-                <NavLink className="menu-link" to="/links" onClick={hide}>Полезные ссылки</NavLink>
-                <NavLink className="menu-link" to="/exams" onClick={hide}>Подготовка к экзаменам</NavLink>
-                <NavLink className="menu-link" to="/vpr" onClick={hide}>ВПР</NavLink>
+        <nav className={isOpen ? 'mobile-nav shown' : 'mobile-nav'}>
+            <div className="mobile-menu__block__column">
+                <NavLink className="mobile-menu__link" to="/" onClick={hide}>Обо мне</NavLink>
+                <NavLink className="mobile-menu__link" to="/links" onClick={hide}>Полезные ссылки</NavLink>
+                <NavLink className="mobile-menu__link" to="/exams" onClick={hide}>Подготовка к экзаменам</NavLink>
+                <NavLink className="mobile-menu__link" to="/vpr" onClick={hide}>ВПР</NavLink>
             </div>
-            <div className="menu-block">
-                <a className="menu-inst" href="https://www.instagram.com/anastasia.pro.geo/" target="_blank" rel="noreferrer"> </a>
-                <a className="menu-mail" href="mailto:anastasia.miasina@ya.ru" target="_blank" rel="noreferrer"> </a>
+            <div className="mobile-menu__block__row">
+                <a className="mobile-menu__inst" href="https://www.instagram.com/anastasia.pro.geo/" target="_blank" rel="noreferrer"> </a>
+                <a className="mobile-menu__mail" href="mailto:anastasia.miasina@ya.ru" target="_blank" rel="noreferrer"> </a>
             </div>
         </nav>
     </>)
